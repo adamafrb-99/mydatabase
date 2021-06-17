7056b63525baf179b3ea0c67c92e708ac5d93f6b
Adam Afriansyah Brahmana <AdamAfriansyah.Brahmana@mitrais.com>
2021-06-17 11:10:01 +0700

    Edit route

--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 const Product = require('../models/Product');
 
+// FILE MANAGEMENT
+const fs = require('fs');
+const uploadConfig = require('../middleware/upload');
+const filePath = 'uploads/products';
+const upload = uploadConfig(filePath);
+
 // GET DATA
 router.get('/', async (req, res) => {
     try {
@@ -26,12 +32,13 @@ router.get("/:id", async (req, res) => {
 });
 
 // ADD DATA
-router.post('/', async (req, res) => {
+router.post('/', upload.single('image'), async (req, res) => {
     try {
         const product = new Product({
             name: req.body.name,
             price: req.body.price,
-            description: req.body.description
+            description: req.body.description,
+            image: req.file.filename
         });
 
         await product.save();
@@ -43,16 +50,24 @@ router.post('/', async (req, res) => {
 });
 
 // EDIT DATA
-router.patch('/:id', async (req, res) => {
+router.patch('/:id', upload.single('image'), async (req, res) => {
     try {
         let product = await Product.findById(req.params.id);
         
         if (req.body.name) product.name = req.body.name;
         if (req.body.price) product.price = req.body.price;
         if (req.body.description) product.description = req.body.description;
+        if (req.file) {
+            fs.unlink(`public/${filePath}/${product.image}`, (err) => {
+                if (err) {
+                    console.log(err.message);
+                }
+            });
+
+            product.image = req.file.filename;
+        } 
 
         await product.save();
-
         res.json(product);
     }
     catch(err) {
@@ -64,6 +79,13 @@ router.patch('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
     try {
         const product = await Product.findById(req.params.id);
+
+        fs.unlink(`public/${filePath}/${product.image}`, (err) => {
+            if (err) {
+                console.log(err.message);
+            }
+        });
+        
         await product.remove();
         res.json({message: "Data has been deleted."});
     }
